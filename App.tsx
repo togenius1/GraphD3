d89a4dcefef392585f5cdf7b76fb582ef00214df
Todsaporn Laosa <togenius1@gmail.com>
2022-08-07 21:36:46 +0700

    add Bar chart

--- App.tsx
+++ App.tsx
@@ -1,27 +1,112 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, Text, View, Dimensions, StatusBar} from 'react-native';
 
-import Graph from './components/Graph';
+import LinearGradient from 'react-native-linear-gradient';
+import {FontAwesomeIcon} from '@fortawesome/react-native-fontawesome';
+import {faBars} from '@fortawesome/free-solid-svg-icons';
 
-const data = [
-  {date: new Date(2018, 9, 1).getTime(), value: 0},
-  {date: new Date(2018, 9, 16).getTime(), value: 0},
-  {date: new Date(2018, 9, 17).getTime(), value: 200},
-  {date: new Date(2018, 10, 1).getTime(), value: 200},
-  {date: new Date(2018, 10, 2).getTime(), value: 300},
-  {date: new Date(2018, 10, 5).getTime(), value: 300},
-];
+import BarGraph from './BarGraph';
 
-export default () => (
-  <View style={styles.container}>
-    <Graph {...{data}} />
-  </View>
-);
+export default function App() {
+  const color = '#FFC77D';
+
+  const data = [
+    {duration: 4, column: 0},
+    {duration: 7, column: 1},
+    {duration: 6, column: 2},
+    {duration: 7, column: 3},
+    {duration: 8, column: 4},
+    {duration: 5, column: 5},
+    {duration: 8, column: 6},
+  ];
+
+  return (
+    <LinearGradient
+      colors={['#282828', '#121212']}
+      style={styles.linearGradient}>
+      <StatusBar
+        barStyle="light-content"
+        hidden={false}
+        backgroundColor="#121212"
+        translucent={true}
+        networkActivityIndicatorVisible={true}
+      />
+
+      <View style={styles.topbar}>
+        <View style={styles.topbar_inner}>
+          <FontAwesomeIcon icon={faBars} style={styles.topbar_hamburger} />
+          <View style={styles.topbar_avatar}></View>
+        </View>
+      </View>
+
+      <View style={styles.spaceBetween}>
+        <View style={styles.header}>
+          <Text style={styles.header_title}>D3 + ART Bar Graph</Text>
+          <Text style={styles.header_sub}>
+            Made by you bc you're a bad ass.
+          </Text>
+        </View>
+
+        <BarGraph color={color} data={data} />
+      </View>
+
+      <View style={styles.footer}></View>
+    </LinearGradient>
+  );
+}
 
 const styles = StyleSheet.create({
-  container: {
+  linearGradient: {
     flex: 1,
-    justifyContent: 'center',
+    height: Math.round(Dimensions.get('window').height),
+    justifyContent: 'space-between',
+  },
+  topbar: {
+    backgroundColor: 'rgba(18,18,18,0.75)',
+    bottom: 2,
+    height: 68,
+  },
+  topbar_inner: {
+    marginTop: 25,
+    paddingTop: 10,
+    paddingBottom: 10,
+    paddingHorizontal: 15,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
     alignItems: 'center',
+    backgroundColor: '#121212',
+  },
+  topbar_hamburger: {
+    padding: 10,
+    color: '#fff',
+  },
+  topbar_avatar: {
+    borderRadius: 100,
+    width: 25,
+    height: 25,
+    borderColor: '#fff',
+    borderWidth: 1,
+  },
+  footer: {
+    height: 70,
+    backgroundColor: '#232323',
+  },
+  header: {
+    paddingTop: 20,
+    paddingLeft: 25,
+  },
+  header_title: {
+    color: '#fff',
+    fontSize: 34,
+    textTransform: 'capitalize',
+  },
+  header_sub: {
+    color: '#fff',
+    fontSize: 16,
+    fontStyle: 'italic',
+  },
+  spaceBetween: {
+    flex: 1,
+    justifyContent: 'space-between',
   },
 });
--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-/**
- * @format
- */
-
+import 'react-native-gesture-handler';
+import React from 'react';
 import {AppRegistry} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 
-AppRegistry.registerComponent(appName, () => App);
+const Root = () => <App />;
+
+AppRegistry.registerComponent(appName, () => Root);
